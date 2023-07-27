3b9c2e4329a163e3f3cce8c8d8dbbbb7707b0908
abi-hong <flffjakfwm20@gmail.com>
2023-07-27 14:07:36 +0900

    number 값만큼 count 변화

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -1,21 +1,37 @@
-import { React, useState } from 'react';
+import { React, useReducer, useState } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  function down() {
-    setCount(count - 1);
+  const [number, setNumber] = useState(1);
+
+  function countReducer(oldCount, action) {
+    if (action.type === 'UP') {
+      return oldCount + action.number;
+    } else if (action.type === 'DOWN') {
+      return oldCount - action.number;
+    } else if (action.type === 'RESET') {
+      return 0;
+    }
   }
-  function reset() {
-    setCount(0);
+  
+  const [count, countDispatch] = useReducer(countReducer, 0); // 1: reducer 함수, 2: 초기값
+  function down() {
+    countDispatch({ type: 'DOWN', number: number });
+  }
+  function reset() { 
+    countDispatch({ type: 'RESET', number: number });
   }
   function up() {
-    setCount(count + 1);
+    countDispatch({ type: 'UP', number: number });
+  }
+  function changeNumber(event) {
+    setNumber(Number(event.target.value));
   }
   return (
     <div>
       <input type="button" value="-" onClick={down} />
       <input type="button" value="0" onClick={reset} />
       <input type="button" value="+" onClick={up} />
+      <input type="text" value={number} onChange={changeNumber} />
       <span>{count}</span>
     </div>
   );
