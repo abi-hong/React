ae5f4544081ab302cadc53a64abe45672ed4f858
abi-hong <flffjakfwm20@gmail.com>
2023-09-03 00:03:57 +0900

    글 생성

--- nextjs/src/app/create/page.tsx
+++ nextjs/src/app/create/page.tsx
@@ -1,5 +1,37 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
     return (
-        <>Create!!</>
+        <form onSubmit={(e: React.FormEvent) => {
+            e.preventDefault();
+            const title = e.target.title.value;
+            const body = e.currentTarget.body.valueOf;
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({title, body})
+            }
+            fetch(`http://localhost/9000/topics`, options)
+                .then((res) => res.json())
+                .then((result) => {
+                    console.log(result);
+                    const lastId = result.id;
+                    router.push(`/read/${lastId}`);
+                })
+        }}>
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="create" />
+            </p>
+        </form>
     )
 }
