92c2d21c6cb99716167775a562b82828da9b0df0
abi-hong <flffjakfwm20@gmail.com>
2023-09-03 21:09:31 +0900

    글 삭제

--- nextjs/src/app/Control.tsx
+++ nextjs/src/app/Control.tsx
@@ -1,16 +1,26 @@
 "use client";
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 export function Control() {
+  const router = useRouter();
   const params = useParams();
   const id = params.id;
+
   return (
     <ul>
       <li><Link href="/create">Create</Link></li>
       {id ? <>
         <li><Link href={`/update/${id}`}>Update</Link></li>
-        <li><input type="button" value="delete" /></li>
+        <li><input type="button" value="delete" onClick={() => {
+          const options = { method: 'DELETE' }
+          fetch(`http://localhost:9000/topics/${id}`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            router.refresh();
+            router.push('/');
+          })
+        }} /></li>
       </> : null}
     </ul>
   );
