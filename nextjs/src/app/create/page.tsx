55fe4df902ef4b11ff10fff3015bf1b631677592
abi-hong <flffjakfwm20@gmail.com>
2023-09-03 20:24:37 +0900

    글 생성, ts에서 이벤트

--- nextjs/src/app/create/page.tsx
+++ nextjs/src/app/create/page.tsx
@@ -1,21 +1,23 @@
 "use client"
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Create() {
+    const [title, setTitle] = useState("");
     const router = useRouter();
     return (
-        <form onSubmit={(e: React.FormEvent) => {
+        <form onSubmit={(e) => {
             e.preventDefault();
-            const title = e.target.title.value;
-            const body = e.currentTarget.body.valueOf;
+            const body = e.currentTarget.body.value;
+            console.log('title', title, 'body', body);
             const options = {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({title, body})
+                body: JSON.stringify({ title, body })
             }
-            fetch(`http://localhost/9000/topics`, options)
+            fetch('http://localhost:9000/topics', options)
                 .then((res) => res.json())
                 .then((result) => {
                     console.log(result);
@@ -24,7 +26,14 @@ export default function Create() {
                 })
         }}>
             <p>
-                <input type="text" name="title" placeholder="title" />
+                <input id='myTitle' type="text" name="title" 
+                    onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                        const {
+                            currentTarget: { value },
+                          } = e;
+                        console.log(e.currentTarget.value);
+                        setTitle(value);
+                }} placeholder="title" />
             </p>
             <p>
                 <textarea name="body" placeholder="body"></textarea>
